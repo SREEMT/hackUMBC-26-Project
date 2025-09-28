60c6ac9a12fd9a8c1e60fff6e088ef5c6e31e865
jonathanDargakis <jmdargakis@loyola.edu>
2025-09-28 01:42:30 -0400

    fixed path to root .env

--- backend/Server.js
+++ backend/Server.js
@@ -1,4 +1,6 @@
-require('dotenv').config();
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '..', '.env') });
+
 
 const dbcon = require('./mongo_setup/models/DbConnection');
 dbcon.connect(); // Connect to MongoDB when starting the server.
