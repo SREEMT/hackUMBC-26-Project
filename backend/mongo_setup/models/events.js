676be10df27a7f6cd07cf1cee3651f6debcd57f8
jonathanDargakis <jmdargakis@loyola.edu>
2025-10-03 22:30:47 -0400

    events tests

--- backend/mongo_setup/models/events.js
+++ backend/mongo_setup/models/events.js
@@ -1,3 +1,12 @@
 const mongoose = require('mongoose');
-const eventSchema = new mongoose.Schema({ name: String, date: Date }, { timestamps: true });
+
+const eventSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    date: { type: Date, required: true }
+  },
+  { timestamps: true }
+);
+
 module.exports = mongoose.model('Event', eventSchema);
+
